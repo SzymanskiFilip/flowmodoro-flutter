71e5c35f6ec7b7d113d399d3f96d9c167a91c34e
Filip <szymanski.fp@gmail.com>
2021-12-05 18:39:30 +0100

    set apps theme to dark theme as default

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import "./themes/themes.dart";
 import "./app.dart";
 
 void main(){
@@ -10,6 +11,7 @@ class Flowmodoro extends StatelessWidget{
   Widget build(BuildContext context) {
     return MaterialApp(
       home: App(),
+      theme: Themes.darkTheme,
     );
   }
 }
