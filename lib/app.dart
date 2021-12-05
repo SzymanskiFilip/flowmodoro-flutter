46a3d1624dcabd835649e20e19dd2900b5a3ce78
Filip <szymanski.fp@gmail.com>
2021-12-05 18:48:03 +0100

    added timer as child of app body

--- lib/app.dart
+++ lib/app.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
+import './widgets/timer.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("hello"),
+        title: Center(
+          child: Text("Flowmodoro Timer"),
+        ),
+      ),
+      body: Center(
+        child: Timer(),
       ),
     );
   }
