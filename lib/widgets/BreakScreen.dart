a08055d62f0fc25f2eabb12912dcbfface504787
Filip <szymanski.fp@gmail.com>
2021-12-12 21:18:43 +0100

    added break screen

---
+++ lib/widgets/BreakScreen.dart
@@ -0,0 +1,15 @@
+import "package:flutter/material.dart";
+
+class BreakScreen extends StatefulWidget {
+  const BreakScreen({Key? key}) : super(key: key);
+
+  @override
+  _BreakScreenState createState() => _BreakScreenState();
+}
+
+class _BreakScreenState extends State<BreakScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
