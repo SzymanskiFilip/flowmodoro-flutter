7147e17f12795e47ce7faf3fce258798676496aa
Filip <szymanski.fp@gmail.com>
2021-12-14 21:26:26 +0100

    deleted debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class Flowmodoro extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: App(),
       theme: Themes.darkTheme,
     );
